c3809714c0893219fa1b280125df849df2a6bf29
Max Woolf <max@minimaxir.com>
2019-10-25 21:34:23 -0700

    file_to_text

--- stylecloud/stylecloud.py
+++ stylecloud/stylecloud.py
@@ -1,4 +1,25 @@
 from icon_font_to_png.icon_font import IconFont
+from wordcloud import WordCloud, STOPWORDS
+import csv
+import os
+
+
+def file_to_text(file_path):
+    """
+    Reads a text file, or if the file is a .csv, read as a list of word/counts.
+    """
+
+    if not file_path.endswith('.csv'):
+        with open(file_path, 'r') as f:
+            text = f.read()
+        return text
+    else:  # parse as a CSV
+        texts = []
+        with open(file_path, 'r') as f:
+            r = csv.reader(f)
+            for row in r:
+                texts.append((row[0], row[1]))
+        return texts
 
 
 def gen_fa_mask(icon_name='fas fa-grin'):
@@ -14,8 +35,8 @@ def gen_fa_mask(icon_name='fas fa-grin'):
     icon_prefix = icon_name.split(' ')[0]
     icon_name_raw = icon_name.split(' ')[1]
 
-    icon = IconFont(css_file='static/fontawesome.min.css',
-                    ttf_file='static/{}'.format(font_files[icon_prefix]))
+    icon = IconFont(css_file=os.path.join('static, fontawesome.min.css'),
+                    ttf_file=os.path.join('static', font_files[icon_prefix]])
 
     icon.export_icon(icon=icon_name_raw[len(icon.common_prefix):],
                      size=300)
