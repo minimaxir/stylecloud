5cdcd8334cce69b6134dec65d79a775d87e8aa6f
Max Woolf <max@minimaxir.com>
2019-10-28 20:58:35 -0700

    gen_stylecloud()

--- stylecloud/stylecloud.py
+++ stylecloud/stylecloud.py
@@ -26,12 +26,12 @@ def file_to_text(file_path):
         return texts
 
 
-def gen_fa_mask(icon_name='fas fa-grin', size=512):
+def gen_fa_mask(icon_name='fas fa-grin', size=1024, icon_dir='.temp'):
     """
     Generates a Font Awesome icon mask from the given FA prefix + name.
     """
 
-    # FA prefixes which map to a text file.
+    # FA prefixes which map to a font file.
     font_files = {'fas': 'fa-solid-900.ttf',
                   'far': 'fa-regular-400.ttf',
                   'fab': 'fa-brands-400.ttf'}
@@ -39,13 +39,13 @@ def gen_fa_mask(icon_name='fas fa-grin', size=512):
     icon_prefix = icon_name.split(' ')[0]
     icon_name_raw = icon_name.split(' ')[1]
 
-    icon = IconFont(css_file=os.path.join('static, fontawesome.min.css'),
+    icon = IconFont(css_file=os.path.join('static', 'fontawesome.min.css'),
                     ttf_file=os.path.join('static', font_files[icon_prefix]))
 
     icon.export_icon(icon=icon_name_raw[len(icon.common_prefix):],
                      size=size,
                      filename="icon.png",
-                     export_dir=".temp")
+                     export_dir=icon_dir)
 
 
 def gen_gradient_mask(size, palette, icon_dir='.temp',
@@ -81,4 +81,35 @@ def gen_gradient_mask(size, palette, icon_dir='.temp',
     mask_array[mask_array != white] = gradient[mask_array != white]
 
     image_colors = ImageColorGenerator(mask_array)
-    return image_colors
+    return image_colors, mask_array
+
+
+def gen_stylecloud(text=None,
+                   size=1024,
+                   icon_name='fas fa-grin',
+                   palette='matplotlib.Viridis_20',
+                   background_color="white",
+                   max_font_size=200,
+                   max_words=2000,
+                   icon_dir='.temp',
+                   output_name='stylecloud.png',
+                   gradient_dir=None,
+                   file_path=None,
+                   random_state=None):
+    """Generates a stylecloud!"""
+
+    assert any([text, file_path]
+               ), "Either text or file_path must be specified."
+
+    gen_fa_mask(icon_name, size, icon_dir)
+    image_colors, mask_array = gen_gradient_mask(size, palette, icon_dir,
+                                                 gradient_dir)
+
+    wc = WordCloud(background_color=background_color,
+                   max_words=max_words, mask=mask_array,
+                   max_font_size=max_font_size, random_state=random_state)
+
+    # generate word cloud
+    wc.generate_from_text(text)
+    wc.recolor(color_func=image_colors, random_state=random_state)
+    wc.to_file(output_name)
